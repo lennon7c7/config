8be0befe0643db98e38687ab9b3d12d7d42e190e
lennon <lennonandjune@gmail.com>
2023-11-16 14:24:12 +0800

    fix: war3 site feifeishijie

--- Others/war3/feifeishijie.php
+++ Others/war3/feifeishijie.php
@@ -1,14 +1,13 @@
 <?php
 /**
  * 下载文件
- *
  * @param string $fileUrl 文件URL
  * @param string $fileName 文件名
  */
 function downloadFile($fileUrl, $fileName)
 {
   // 设置文件保存路径
-  $savePath = './' . $fileName;
+  $savePath = './maps/' . $fileName;
 
   // 打开远程文件
   $file = fopen($fileUrl, 'rb');
@@ -26,30 +25,69 @@ function downloadFile($fileUrl, $fileName)
       // 关闭文件句柄
       fclose($saveFile);
     } else {
-      echo '无法创建本地文件！';
+      echo "    $fileUrl 无法创建本地文件！", PHP_EOL;
     }
 
     // 关闭文件句柄
     fclose($file);
 
-    echo '文件下载完成！';
+//    echo '文件下载完成！';
   } else {
-    echo '无法打开远程文件！';
+    echo "    $fileUrl 无法打开远程文件！", PHP_EOL;
   }
 }
 
-$min = 11110;
-$max = 19115;
-
-
+$min = 1;
+$max = 609;
 for ($i = $min; $i <= $max; $i++) {
-  // 获取文件URL
-  $fileUrl = "http://www.feifeishijie.com/e/enews/?enews=DownSoft&classid=63&id=$i&pathid=0&pass=25e9384f38ad5f8a67011a8fce80a563&p=:::";
+  if ($i == 1) {
+    $page_list_url = 'http://www.feifeishijie.com/fangshou/';
+  } else {
+    $page_list_url = "http://www.feifeishijie.com/fangshou/index_$i.html";
+  }
 
-// 设置文件名
-  $fileName = "$i.rar";
-  echo $fileName . PHP_EOL;
-// 执行下载
-  downloadFile($fileUrl, $fileName);
+  echo $page_list_url, PHP_EOL;
+  $html = @file_get_contents($page_list_url);
+  $needle = '您访问的页面不在';
+  if (strpos($html, $needle) !== false) {
+    echo $needle, PHP_EOL;
+    exit();
+  }
+
+  // 创建DOM对象
+  $dom = new DOMDocument();
+  @$dom->loadHTML($html);
+
+  $elements = $dom->getElementsByTagName('a');
+
+  // 遍历元素
+  foreach ($elements as $element) {
+    // 获取href属性值和title属性值
+    $href = $element->getAttribute('href');
+    $title = $element->getAttribute('title');
+    // 删除地图下载
+    $title = str_replace('地图下载', '', $title);
+    $title = str_replace('下载', '', $title);
+
+    echo "  $href $title", PHP_EOL;
+    if ($element->getAttribute('class') != 'more' || $href == '' || $title == '') {
+//      echo "    $href $title", PHP_EOL;
+      continue;
+    }
+
+    $id = substr($href, strrpos($href, '/') + 1, strrpos($href, '.') - strrpos($href, '/') - 1);
+    if (!is_numeric($id)) {
+      echo "    id: $id", PHP_EOL;
+      continue;
+    }
+
+    // 获取文件URL
+    $fileurl = "http://www.feifeishijie.com/e/enews/?enews=DownSoft&classid=63&id=$id&pathid=0&pass=25e9384f38ad5f8a67011a8fce80a563&p=:::";
+
+    // 设置文件名
+    $filename = "$id-$title.rar";
+    echo "    $filename", PHP_EOL;
+    // 执行下载
+    downloadFile($fileurl, $filename);
+  }
 }
-
